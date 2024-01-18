4eeded4b7fd1792d13dedbe8c8ca701ceb73b1b5
Hérson Rezende <herson.academico@gmail.com>
2024-01-18 12:58:53 -0300

    chore(): moving advisor package

--- src/modules/advisor/entities/advisor.entity.ts
+++ src/modules/advisor/entities/advisor.entity.ts
@@ -1,4 +1,4 @@
-import { User } from '../../user/interface/user.interface'
+import { User } from '../../../user/interface/user.interface'
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
 
 @Entity('advisor')
