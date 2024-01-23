7fa99de1afec8a6cd78b0e746d9b69c3be94d6f2
Hérson Rezende <herson.academico@gmail.com>
2024-01-23 16:43:20 -0300

    chore(notification): removendo modulo desnecessario de Student

--- src/services/ending-scholarship-notification/notification.module.ts
+++ src/services/ending-scholarship-notification/notification.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { ScholarshipModule } from '../../modules/scholarship/scholarship.module'
 import { EmailModule } from '../email-sending/email.module'
 import { NotificationService } from './service/notification.service'
-import { StudentModule } from '../../modules/student/student.module'
 
 @Module({
-  imports: [EmailModule, ScholarshipModule, StudentModule],
+  imports: [
+    EmailModule, 
+    ScholarshipModule,
+  ],
   providers: [NotificationService],
-  exports: []
 })
 export class NotificationModule {}
