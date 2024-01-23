99ed6ad13a740f585e4531e9073e969042b3a089
Hérson Rezende <herson.academico@gmail.com>
2024-01-23 16:08:07 -0300

    feat(enrollment): melhoria no mapper

--- src/modules/enrollment/mappers/enrollment.mapper.ts
+++ src/modules/enrollment/mappers/enrollment.mapper.ts
@@ -1,7 +1,7 @@
-import { ScholarshipMapper } from "../../scholarship/mapper/scholarship.mapper";
 import { Enrollment } from "../entities/enrollment.entity";
 import { AdvisorMapper } from "../../advisor/mapper/advisor.mapper";
-import { toResponseStudentDto } from "../../student/mapper/student.mapper";
+import { StudentMapper } from "../../student/mapper/student.mapper";
+import { ScholarshipMapper } from "../../scholarship/mapper/scholarship.mapper";
 
 export class EnrollmentMapper {
     static simplified(enrollment: Enrollment) {
@@ -16,24 +16,21 @@ export class EnrollmentMapper {
     }
 
     static detailed(enrollment: Enrollment) {
+        const simplified = this.simplified(enrollment)
+
         return {
-            id: enrollment.id,
-            student_id: enrollment.student_id,
-            advisor_id: enrollment.advisor_id,
-            active: enrollment.active,
+            ...simplified,
             enrollment_date: enrollment.enrollment_date,
             enrollment_number: enrollment.enrollment_number,
             enrollment_program: enrollment.enrollment_program,
             defense_prediction_date: enrollment.defense_prediction_date,
-            created_at: enrollment.created_at,
-            updated_at: enrollment.updated_at,
         }
     }
 
     static detailedWithRelations(enrollment: Enrollment) {
         const detailed = this.detailed(enrollment)
         const advisor = enrollment.advisor ? AdvisorMapper.detailed(enrollment.advisor) : null
-        const student = enrollment.student ? toResponseStudentDto(enrollment.student) : null
+        const student = enrollment.student ? StudentMapper.detailed(enrollment.student) : null
         const scholarships = enrollment.scholarships?.map((scholarship) => ScholarshipMapper.detailed(scholarship))
 
         return {
