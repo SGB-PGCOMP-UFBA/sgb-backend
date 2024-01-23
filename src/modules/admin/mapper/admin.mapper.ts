14b2bcbb3a241188fa991b1147d87a484cbbd388
Hérson Rezende <herson.academico@gmail.com>
2024-01-23 16:12:57 -0300

    feat(admin): melhoria no mapper

--- src/modules/admin/mapper/admin.mapper.ts
+++ src/modules/admin/mapper/admin.mapper.ts
@@ -1,24 +1,23 @@
 import { Admin } from "../entities/admin.entity";
 
 export class AdminMapper {
-  static simplified(admin: Admin) {
-      return {
-          id: admin.id,
-          role: admin.role,
-          created_at: admin.created_at,
-          updated_at: admin.updated_at,
-      }
-  }
+    static simplified(admin: Admin) {
+        return {
+            id: admin.id,
+            role: admin.role,
+            created_at: admin.created_at,
+            updated_at: admin.updated_at,
+        }
+    }
 
-  static detailed(admin: Admin) {
-      return {
-          id: admin.id,
-          role: admin.role,
-          created_at: admin.created_at,
-          updated_at: admin.updated_at,
-          tax_id: admin.tax_id,
-          name: admin.name,
-          email: admin.email,
-      }
-  }
+    static detailed(admin: Admin) {
+        const simplified = this.simplified(admin)
+
+        return {
+            ...simplified,
+            tax_id: admin.tax_id,
+            name: admin.name,
+            email: admin.email,
+        }
+    }
 }
