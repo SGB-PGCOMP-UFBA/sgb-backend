c5f273f33abf94326d57a74407a46954cdcc474f
Hérson Rezende <herson.academico@gmail.com>
2024-09-10 20:47:36 -0300

    feat(): nova migration para ajustar numero de matricula

---
+++ src/core/database/migrations/1725214155800-updateTableEnrollment.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
+
+export class updateTableEnrollment1725214155800 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'enrollment',
+      'enrollment_number',
+      new TableColumn({
+        name: 'enrollment_number',
+        type: 'char',
+        length: '15',
+        isUnique: true,
+        isNullable: false
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'enrollment',
+      'enrollment_number',
+      new TableColumn({
+        name: 'enrollment_number',
+        type: 'char',
+        length: '10',
+        isUnique: true,
+        isNullable: false
+      })
+    )
+  }
+}
